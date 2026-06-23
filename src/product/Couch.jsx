6b47df4d0ac7eb6ab19c1adba2850978de6b3778
sathish-24502 <sathisheswari24@gmail.com>
2026-06-23 20:42:06 +0530

    Update project 3nd time

---
+++ src/product/Couch.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import sofa1 from '../assets/sofa1.jpg'
+import sofa2 from '../assets/sofa2.jpg'
+import sofa3 from '../assets/sofa3.jpg'
+import sofaaAd from '../assets/Sofa-adImage.jpg'
+import sofaAd2 from '../assets/sofa-adImge.jpg'
+const Couch = () => {
+
+     const sofaImages = [
+        {
+            
+        }
+    ]
+  return (
+
+   
+    <>
+        
+    </>
+  )
+}
+
+export default Couch
