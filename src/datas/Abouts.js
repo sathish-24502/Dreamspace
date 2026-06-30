b098dc0eb34722957898f13732a797726090b486
sathish-24502 <sathisheswari24@gmail.com>
2026-06-30 20:18:11 +0530

    Updated About Page

--- src/datas/Abouts.js
+++ src/datas/Abouts.js
@@ -1,40 +1,79 @@
-import About from "../pages/About"
+import About from "../pages/About";
+import person1 from "../assets/Images/person-1.png";
+import person2 from "../assets/Images/person-2.jpg";
+import person3 from "../assets/Images/person-3.jpg";
+import person4 from "../assets/Images/person-4.jpg";
+import person5 from "../assets/Images/person-5.jpg";
 
-const about =[
-        {
-            name : "DreamS...",
-            founder :"SATHISHWARAN_S",
-            email : "dreams2705@gmail.com",
-            phone : "2518655-8658",
-            since : "27/05/2026",
-            works:"Crafting Comfort, Designing Dreams",      
-            desc :"At DreamS..., we believe that furniture is more than just décor—it's a part of your lifestyle. We offer modern, stylish, and comfortable furniture designed to transform every house into a dream home.",
-            sofa:"Premium Sofa",
-            beds:"Comfortable Bed",
-            furniture:"Storage Solution",
-            
-        },
+const about = [
+  {
+    image: person1,
+    name: "Mr.SATHISHWARAN.S",
+    title: " DreamS... - Founder & CEO",
+    desc :"Visionary leader who established the company and drives its long-term growth and innovation.",
+    btn: "Learn More",
+    
+  },
+  {
+    image: person2,
+    name: "Ryan Mitchell",
+    title: "General Manager",
+    desc :"Oversees daily operations, business performance, and strategic decision-making across departments.",
+    btn: "Learn More",
+  },
+  {
+    image: person3,
+    name: "James Walker",
+    title: "Senior Developer",
+    desc :"Leads software development, designs scalable solutions, and mentors the development team.",
+    btn: "Learn More",
+  },
+  {
+    image: person4,
+    name: "Daniel Foster",
+    title: "Testing Team Lead",
+    desc :"Ensures software quality by managing testing processes, leading QA teams, and delivering reliable products.",
+    btn: "Learn More",
+  },
+  {
+    image: person5,
+    name: "Emma Carter",
+    title: "Marketing Manager",
+    desc :"Develops and executes marketing strategies to strengthen brand awareness and drive customer engagement.",
+    btn: "Learn More",
+  },
 
-    ]
-   
-    const WhyUs = [
-        {
-            point1 :"Premium Quality Materials",
-            point2 :"Modern & Elegant Designs",
-            point3 :"Affordable Prices",
-            point4 :"Fast & Secure Delivery",
-            point5 :"Excellent Customer Support",
-            point6 :"Dedicated Customer Support",
-            point7 :"Secure Shopping",
-            point8 :"Wide Range of Products",
-            point9 :"Eco-Friendly Materials",
-            point10 :"Expert Craftsmanship",
-            point11 :"ECustomer Satisfaction",
-            point12 :"Fast & Safe Delivery",
-        }
-    ]
+  // {
+  //     name : "DreamS...",
+  //     founder :"SATHISHWARAN_S",
+  //     email : "dreams2705@gmail.com",
+  //     phone : "2518655-8658",
+  //     since : "27/05/2026",
+  //     works:"Crafting Comfort, Designing Dreams",
+  //     desc :"At DreamS..., we believe that furniture is more than just décor—it's a part of your lifestyle. We offer modern, stylish, and comfortable furniture designed to transform every house into a dream home.",
+  //     sofa:"Premium Sofa",
+  //     beds:"Comfortable Bed",
+  //     furniture:"Storage Solution",
 
+  // },
+];
+console.log(about);
 
+const WhyUs = [
+  {
+    point1: "Premium Quality Materials",
+    point2: "Modern & Elegant Designs",
+    point3: "Affordable Prices",
+    point4: "Fast & Secure Delivery",
+    point5: "Excellent Customer Support",
+    point6: "Dedicated Customer Support",
+    point7: "Secure Shopping",
+    point8: "Wide Range of Products",
+    point9: "Eco-Friendly Materials",
+    point10: "Expert Craftsmanship",
+    point11: "ECustomer Satisfaction",
+    point12: "Fast & Safe Delivery",
+  },
+];
 
-   export { about , WhyUs };
-  
+export { about, WhyUs };
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,11 @@ import Navbar from '../components/Navbar'
 import {about , WhyUs} from '../datas/Abouts'
 import Adimge from '../assets/Images/AD-Image.jpg'
 import dots from '../assets/dots-yellow.svg'
+import FooterEnd from '../components/FooterEnd'
+import Footer from '../components/Footer'
+import { Link } from 'react-router-dom'
+import Service from '../components/Service'
+
 
 const About = () => {
 
@@ -11,10 +16,13 @@ const About = () => {
   return (
     <>
     <Navbar/>
-    <span className='flex bg-orange-400'>
-        <h1 className='text-4xl font-bold font-sora p-24'>About</h1>
+    <span className='flex flex-col font-poppins p-16 gap-2 bg-orange-400'>
+        <h1 className='text-4xl font-bold font-sora'>About Us</h1>
+        <p className='w-1/2'>DreamS... is your trusted destination for premium furniture that blends comfort, style, and quality. Our mission is to help you create a home you'll love.</p>
+        <Link className='mt-6 font-semibold w-32 flex justify-center items-center p-1 rounded-lg hover:shadow-white bg-orange-800 text-white shadow-sm'>Shop Now</Link>
+
     </span>
-        <div className='p-10'>
+        {/* <div className='p-10'>
             {about.map((item , indx)=>(
               <div key={indx} className='grid gap-4 font-poppins mr-8 mt-12' >
                <span className='flex items-center justify-around w-1/2'>
@@ -65,11 +73,27 @@ const About = () => {
               </div>
 
             ))}
+        </div> */}
+        <span className='font-poppins '>
+          <h1 className='text-3xl font-bold text-gray-700 p-6'>Our Team</h1>
+        </span>
+        <div className='grid grid-cols-3 justify-center items-center p-4 mt-4 mb-6 gap-6 font-poppins'>
+          {about.map((about , indx) => (
+            <div key={indx} className='grid justify-center items-center gap-2'>
+              <img src={about.image} alt="Person Picture" className='w-72 h-96 rounded-xl shadow-sm shadow-orange-300'/>
+              <h1 className='font-bold text-xl'>{about.name}</h1>
+              <h2 className='font-bold text-gray-700'>{about.title}</h2>
+              <p className='text-gray-600 w-72'>{about.desc}</p>
+              <p className='underline text-gray-600'>{about.btn}</p>
+            </div>
+          ))}
         </div>
+            <Service/>
 
-        <div className='mt-44 mb-10'>
+
+        <div className='mt-10 mb-10 grid gap-2'>
           <span className='flex flex-col justify-center items-center gap-2'>
-            <h1 className='text-3xl font-semibold text-gray-600 mb-4'>Why Choose Us</h1>
+            {/* <h1 className='text-3xl font-semibold text-gray-600 mb-4'>Why Choose Us</h1> */}
             <p className='w-3/4 text-gray-700'>At DreamS..., we combine quality craftsmanship, modern design, and affordable pricing to create furniture that transforms your house into a home. With reliable service and customer-first support, we're committed to making every shopping experience comfortable and enjoyable.</p>
           </span>
           {WhyUs.map((item, index) => (
@@ -90,6 +114,8 @@ const About = () => {
   </ul>
 ))}
         </div>
+          <Footer/>
+        <FooterEnd/>
     </>
   )
 }
